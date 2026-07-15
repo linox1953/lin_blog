4cfa85dc73c160a4428ec61d90f1dfa4145dfb3e
Linox <linox1953@outlook.com>
2026-07-15 13:42:03 +0800

    format files

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -81,9 +81,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 主页横幅主标题字体大小
 			titleSize: "3.8rem",
 			// 主页横幅副标题
-			subtitle: [
-				"Lin Lijian",
-			],
+			subtitle: ["Lin Lijian"],
 			// 主页横幅副标题字体大小
 			subtitleSize: "1.5rem",
 			typewriter: {
--- src/config/fontConfig.ts
+++ src/config/fontConfig.ts
@@ -123,6 +123,6 @@ export const fontConfig: FontSelectionConfig = {
 		},
 		"--font-cinzel": {
 			extraChars: "",
-		}
+		},
 	},
 };
