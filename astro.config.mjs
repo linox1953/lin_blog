0f483e991b445ee1068163a455f698ca4ab9e47d
Linox <linox1953@outlook.com>
2026-07-15 15:28:02 +0800

    modify astro.config to revert changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -64,9 +64,9 @@ const adapter = process.env.CF_WORKERS
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.ASTRO_SITE || siteConfig.site_url,
+	site: siteConfig.site_url,
 
-	base: process.env.ASTRO_BASE || "/",
+	base: "/",
 	trailingSlash: "always",
 
 	// 字体配置 - 只加载实际使用的字体，跳过未引用的以加快构建
